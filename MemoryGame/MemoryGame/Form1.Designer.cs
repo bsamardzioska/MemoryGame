0171a9e63cfca7503c8939b5859833cf93c1aac7
Мартин Павловски <pavlovski.martin@students.finki.ukim.mk>
2014-05-11 17:47:32 +0100

    MemoryGame commit
    MemoryGame commit

---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip4 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip4
+            // 
+            this.menuStrip4.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip4.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.menuStrip4.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip4.Name = "menuStrip4";
+            this.menuStrip4.Size = new System.Drawing.Size(562, 24);
+            this.menuStrip4.TabIndex = 2;
+            this.menuStrip4.Text = "menuStrip4";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem11,
+            this.toolStripMenuItem12});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem11
+            // 
+            this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(132, 22);
+            this.toolStripMenuItem11.Text = "New Game";
+            this.toolStripMenuItem11.Click += new System.EventHandler(this.toolStripMenuItem11_Click);
+            // 
+            // toolStripMenuItem12
+            // 
+            this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(132, 22);
+            this.toolStripMenuItem12.Text = "Exit";
+            this.toolStripMenuItem12.Click += new System.EventHandler(this.toolStripMenuItem12_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem7});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem2.Text = "Options";
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9,
+            this.toolStripMenuItem10});
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(128, 22);
+            this.toolStripMenuItem7.Text = "Board Size";
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.CheckOnClick = true;
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(97, 22);
+            this.toolStripMenuItem8.Text = "2 x 3";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.CheckOnClick = true;
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(97, 22);
+            this.toolStripMenuItem9.Text = "6 x 6";
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.toolStripMenuItem9_Click);
+            // 
+            // toolStripMenuItem10
+            // 
+            this.toolStripMenuItem10.CheckOnClick = true;
+            this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(97, 22);
+            this.toolStripMenuItem10.Text = "8 x 9";
+            this.toolStripMenuItem10.Click += new System.EventHandler(this.toolStripMenuItem10_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem3.Text = "Help";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(163, 22);
+            this.toolStripMenuItem5.Text = "Instructions";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(163, 22);
+            this.toolStripMenuItem6.Text = "About this Game";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 396);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(562, 420);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip4);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip4.ResumeLayout(false);
+            this.menuStrip4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boardSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x6ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x9ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutThisGameToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        
+        Color[] colorsList = new Color[]{
+            Color.Red,Color.Blue, Color.Aqua, Color.Green, Color.Brown, Color.Beige,
+            Color.DarkBlue, Color.Orange, Color.Olive, Color.Yellow,
+            Color.LemonChiffon, Color.Lavender, Color.Indigo, Color.HotPink,
+            Color.Gray, Color.Fuchsia, Color.ForestGreen, Color.Firebrick,
+            Color.DodgerBlue, Color.DeepSkyBlue, Color.Violet, Color.Tomato,
+            Color.Teal, Color.SpringGreen, Color.Wheat, Color.Turquoise,
+            Color.Chartreuse, Color.DarkKhaki, Color.DarkOrange, Color.DarkSeaGreen,
+            Color.DeepPink, Color.DodgerBlue, Color.Gainsboro, Color.Gold, 
+            Color.SteelBlue,Color.Tan
+        };
+        private int x = 0;
+        private int y = 0;
+        private int a = 0;
+        private int b = 0;
+        private Button [,] buttons;
+        private bool[] colorUsed = new bool[72];
+        private ArrayList colors = new ArrayList();
+        private int counter = 0;
+        private Button prevButton = null;
+        private int pairs = 0;
+        private int totalNumPairs;
+        public int boardSize;
+        private Random ran = new Random();
+        ProgressBar pbTime;
+        Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetColors();
+            totalNumPairs = 3;
+            boardSize = 0;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+        }
+
+        private void resetColors()
+        {
+            for (int u = 0; u < 72; u++)
+            {
+                colorUsed[u] = false;
+            } 
+        }
+
+        // Funkcija koja se koristi za odbiranje na pozadinska boja za 
+        // kvadratite. Funkcijata koristi generator na slucajni broevi
+        // za da opredeli indeks za bojata. Indeksot sluzi za pristap do
+        // poleto colorsList i zemanje na soodvetnata boja od tamu.
+        // Poleto colorsUsed gi sodrzi site boi za inicijalizacija.
+        // Nizata colorUsed sluzi za oznacuvanje koja od boite vo colorsList
+        // e zafatena. Sekoja od boite vo colorsList moze da se javi najmnogu
+        // dvapati na nacrtanite kvadrati.
+        
+        private Color colorFunc()
+        {
+            int index=0;
+            index=ran.Next(0,x*y/2);
+            while (colorUsed[index] == true)
+            {
+                if (colorUsed[index + (x * y / 2)] == true)
+                    index = ran.Next(0, x * y / 2);
+                else 
+                {
+                    colorUsed[index + (x * y / 2)] = true;
+                    return colorsList[index];
+                }
+            }
+            colorUsed[index] = true;
+            return colorsList[index];
+        }
+
+        //Funkcija koja sluzi za iscrtuvanje na kvadratite.
+        //Sekoj kvadrat e objekt od klasata Button. Prvo se
+        //kreira matrica od kopcinja so odbranite dimenzii (x i y).
+        //Potoa poedinecno se kreira sekoe kopce, se postavuvaat 
+        //negovite dimenzii, pocetna pozicija, ime, se dodava
+        //nastan za klikanje na istoto i se postavuvaat vrednostite za progress-bar.   	  
+        //Istovremeno, se odbira bojata koja ke bide povrzana so soodvetniot 
+        //kvadrat (kopce), potoa istiot se dodava na panel.
+
+        private void CreateTable()
+        {
+            buttons = new Button[x, y];
+            a = (this.Width / 2) - (x / 2) * 60;
+            b = (this.Height / 2) - (y / 2) * 60;
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    buttons[i, j] = new Button
+                    {
+                        Height = 50,
+                        Width = 50,
+                    };
+                    Point p = new Point(a + i * 60, b + j * 60);
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Location = p;
+                    buttons[i, j].Name = "btn" + i.ToString() + j.ToString();
+                    buttons[i, j].Click += Form1_Click;
+                    colors.Add(colorFunc());
+                    panel1.Controls.Add(buttons[i, j]);
+                }
+            }
+            panel1.Controls.Add(GetTimeLabel());
+            pbTime = new ProgressBar
+            {
+                Height = 30,
+                Width = 50*x + 10*(x-1),
+            };
+            pbTime.Location = new Point(a, b + y * 60 + 18);
+            pbTime.Minimum = 0;
+            if(boardSize == 0)
+                pbTime.Maximum = 60;
+            else if (boardSize == 1)
+                pbTime.Maximum = 120;
+            else pbTime.Maximum = 300;
+            pbTime.Value = 0;
+            panel1.Controls.Add(pbTime);
+            timer.Stop();
+            timer.Start();
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            Button btn = sender as Button;
+            string i = btn.Name.Substring(3, 1);
+            string j = btn.Name.Substring(4, 1);
+            btn.BackColor = (Color) colors[Int32.Parse(i)*y+Int32.Parse(j)];
+            btn.Refresh();
+            if (counter == 2)
+            {
+                if (btn.BackColor.Equals(prevButton.BackColor))
+                {
+                    System.Threading.Thread.Sleep(500);
+                    prevButton.BackColor = Color.Black;
+                    btn.BackColor = Color.Black;
+                    pairs++;
+                    if (pairs == totalNumPairs)
+                    {
+                        pairs = 0;
+                        timer.Stop();
+                        MessageBox.Show("You won!");
+                        pbTime.Value = 0;
+                    }
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    btn.BackColor = Color.LightGray;
+                    prevButton.Enabled = true;
+                    prevButton.BackColor = Color.LightGray;
+                }
+                counter = 0;
+            }
+            else
+            {
+                prevButton = btn;
+                btn.Enabled = false;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (pbTime.Value == pbTime.Maximum)
+            {
+                timer.Stop();
+                MessageBox.Show("You lost!");
+                panel1.Controls.Clear();             
+            }
+            else pbTime.Value++;
+        }
+
+        private void toolStripMenuItem12_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click on any rectangle card to uncover the card content. With each play you can only turn over two cards. If the content of the two cards match, then repeat the process until the board is cleared out.");
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+             ProcessStartInfo AboutThisGame = new ProcessStartInfo("http://en.wikipedia.org/wiki/Concentration_(game)");  
+             Process.Start(AboutThisGame);
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e) //2 x 3
+        {
+            if (toolStripMenuItem10.Checked)
+                toolStripMenuItem10.Checked = false;
+            if (toolStripMenuItem9.Checked)
+                toolStripMenuItem9.Checked = false;
+            panel1.Controls.Clear();
+            toolStripMenuItem8.Checked = true;
+            x = 3;
+            y = 2;
+            resetColors();
+            CreateTable();
+            totalNumPairs = (x * y) / 2;
+            boardSize = 0;
+            panel1.Controls.Add(GetBoardSizeLabel());
+        }
+
+        private void toolStripMenuItem10_Click(object sender, EventArgs e) //8 x 9
+        {
+            if (toolStripMenuItem8.Checked)
+                toolStripMenuItem8.Checked = false;
+            if (toolStripMenuItem9.Checked)
+                toolStripMenuItem9.Checked = false;
+            panel1.Controls.Clear();
+            toolStripMenuItem10.Checked = true;
+            x = 9;
+            y = 8;
+            resetColors();
+            CreateTable();
+            totalNumPairs = (x * y) / 2;
+            boardSize = 2;
+            panel1.Controls.Add(GetBoardSizeLabel());
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e) // 6 x 6
+        {
+            if (toolStripMenuItem10.Checked)
+                toolStripMenuItem10.Checked = false;
+            if (toolStripMenuItem8.Checked)
+                toolStripMenuItem8.Checked = false;
+            panel1.Controls.Clear();
+            toolStripMenuItem9.Checked = true;
+            x = 6;
+            y = 6;
+            resetColors();
+            CreateTable();
+            totalNumPairs = (x * y) / 2;
+            boardSize = 1;
+            panel1.Controls.Add(GetBoardSizeLabel());
+        }
+
+        private void toolStripMenuItem11_Click(object sender, EventArgs e)
+        {
+            if (toolStripMenuItem10.Checked)
+                toolStripMenuItem10.Checked = false;
+            if (toolStripMenuItem9.Checked)
+                toolStripMenuItem9.Checked = false;
+            panel1.Controls.Clear();
+            toolStripMenuItem8.Checked = true;
+            x = 3;
+            y = 2;
+            resetColors();
+            CreateTable();
+            panel1.Controls.Add(GetBoardSizeLabel());
+        }
+
+        //Funkcija koja sluzi za prikazuvanje na labela so
+        //soodvetniot broj na izbrani kvadrati na formata.
+        //Za najmala, sredna i golema tabla soodvetno se 
+        //postaveni vrednostite 0, 1 i 2.
+
+        private Label GetBoardSizeLabel()
+        {
+            Label lblBoardSize = new Label();
+            lblBoardSize.Location = new Point(a - 4, b - 25);
+            lblBoardSize.Width = 200;
+            string boardSizeStr = string.Empty;
+            if (boardSize == 0)
+                boardSizeStr += "2 x 3";
+            else if (boardSize == 1)
+                boardSizeStr += "6 x 6";
+            else boardSizeStr += "8 x 9";
+            lblBoardSize.Text = "Големина на табла: " + boardSizeStr;
+            lblBoardSize.Font = new Font("Calibri", 12);
+            return lblBoardSize;
+        }
+        
+        //Funkcija koja se koristi za postavuvanje na labela nad 
+        //preostanatoto vreme za igranje, na soodvetna pozicija.
+        private Label GetTimeLabel()
+        {
+            Label timeLabel = new Label();
+            timeLabel.Text = "Преостанато време:";
+            timeLabel.Width = 200;
+            timeLabel.Location = new Point(a - 2, b + y * 60 - 5);
+            timeLabel.Font = new Font("Calibri", 9);
+            return timeLabel;
+        }
+    }
+}
